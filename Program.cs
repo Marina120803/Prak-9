65051ac7403f207196605d51147533f4fee20eaf
ACER <ACER@DESKTOP-O7A416V>
2022-10-28 15:31:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace aip;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите цену товара: ");
+        int x = int.Parse(Console.ReadLine());
+        int i = 10;
+
+        do
+        {
+            Console.WriteLine("{0} штук товара стоят {1} руб", i, i * x);
+            i += 10;
+        }
+        while (i <= 200);
+        Console.ReadKey();
+    }
+}
